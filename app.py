06605452d2e0323ca3a1be85807d6431bdb66d4b
Angela Lin <angela97lin@gmail.com>
2014-10-29 13:16:28 -0400

    ahhhhhh

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST","GET"])
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about", methods=["POST","GET"])
+def about():
+    return render_template("about.html")
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
